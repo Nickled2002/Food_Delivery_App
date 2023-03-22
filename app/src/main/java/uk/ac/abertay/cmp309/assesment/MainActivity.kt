99d4c9ed47403a7e5927ef6a516a5256ef427965
Nickled2002 <64671430+Nickled2002@users.noreply.github.com>
2023-03-22 18:05:42 +0000

    Sign Up and Login done

--- app/src/main/java/uk/ac/abertay/cmp309/assesment/MainActivity.kt
+++ app/src/main/java/uk/ac/abertay/cmp309/assesment/MainActivity.kt
@@ -1,11 +1,61 @@
 package uk.ac.abertay.cmp309.assesment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var Email: EditText
+    private lateinit var Password: EditText
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Email = findViewById(R.id.Input_Email)
+        Password = findViewById(R.id.Input_Pass)
+        firebaseAuth = FirebaseAuth.getInstance()
+    }
+
+    fun onClick(view: View?) {
+
+        if (view != null) {
+
+
+            when (view.id) {
+                R.id.TextButton_SignUp -> {
+                    val intent = Intent(this, SignUpActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.Button_Login -> {
+                    val Inemail = Email.text.toString()
+                    val Inpass = Password.text.toString()
+                    if (Inemail.isNotEmpty() && Inpass.isNotEmpty()) {
+                        firebaseAuth.signInWithEmailAndPassword(Inemail,Inpass).addOnCompleteListener{
+                            if (it.isSuccessful){
+                                Toast.makeText(this, "You Logged in", Toast.LENGTH_SHORT).show()
+                            }else{
+                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }else{
+                        Toast.makeText(this, "Incorrect password or email", Toast.LENGTH_SHORT).show()
+                    }
+
+
+                }
+
+            }
+        }
     }
 }
+
+
+
--- app/src/main/java/uk/ac/abertay/cmp309/assesment/SignUpActivity.kt
+++ app/src/main/java/uk/ac/abertay/cmp309/assesment/SignUpActivity.kt
@@ -1,11 +1,67 @@
 package uk.ac.abertay.cmp309.assesment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var Email: EditText
+    private lateinit var CPassword: EditText
+    private lateinit var Password: EditText
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        Email = findViewById(R.id.SignUp_Input_Email)
+        Password = findViewById(R.id.SignUp_Input_Password)
+        CPassword = findViewById(R.id.SignUp_input_CPassword)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+    }
+
+    fun onClick(view: View?) {
+
+        if (view != null) {
+            when(view.id) {
+                R.id.TextButton_SignUp -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.SignUp_Button_SignUp -> {
+                    val Inemail = Email.text.toString()
+                    val Inpass = Password.text.toString()
+                    val Incpass = CPassword.text.toString()
+                    if (Inemail.isNotEmpty() && Inpass.isNotEmpty() && Incpass.isNotEmpty() ) {
+                        if (Inpass == Incpass){
+
+                            firebaseAuth.createUserWithEmailAndPassword(Inemail, Inpass).addOnCompleteListener{
+                                if(it.isSuccessful){
+                                    val intent = Intent(this, MainActivity::class.java)
+                                    startActivity(intent)
+                                }else{
+                                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        }else{
+                            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT).show()
+                        }
+                    }else {
+                        Toast.makeText(this, "Fields are empty", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
