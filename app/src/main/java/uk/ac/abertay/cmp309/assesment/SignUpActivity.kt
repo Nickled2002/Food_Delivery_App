def4f1ed1387a8f0dcce4f3d1b2ab3b818337dad
Nickled2002 <64671430+Nickled2002@users.noreply.github.com>
2023-03-24 14:27:27 +0000

    Extra fields in Sign up

--- app/src/main/java/uk/ac/abertay/cmp309/assesment/SignUpActivity.kt
+++ app/src/main/java/uk/ac/abertay/cmp309/assesment/SignUpActivity.kt
@@ -4,11 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -48,6 +48,9 @@ class SignUpActivity : AppCompatActivity() {
 
                             firebaseAuth.createUserWithEmailAndPassword(Inemail, Inpass).addOnCompleteListener{
                                 if(it.isSuccessful){
+                                    val user = FirebaseAuth.getInstance().currentUser
+                                    val db = Firebase.firestore
+
                                     val intent = Intent(this, MainActivity::class.java)
                                     startActivity(intent)
                                 }else{
